ad945ac3a05e5068c039f2dabfe13b443835dc9e
kabarharian <ipadnn.com@gmail.com>
2025-08-08 22:22:45 +0700

    push

--- functions/gemini.js
+++ functions/gemini.js
@@ -1,8 +1,8 @@
+import { onRequestPost } from "./gemini";
+
 export const onRequest = async (context) => {
   if (context.request.method === "POST") {
     return await onRequestPost(context);
   }
   return new Response("Method Not Allowed", { status: 405 });
 };
-
-import { onRequestPost } from "./gemini";
